d927715220e77393f3a2ddd0a2a22ec0ef8e62c5
razbi <razhaim101@gmail.com>
2023-01-13 22:28:29 +0200

    start working on part b

--- src/IllegalReleaseAttempt.java
+++ src/IllegalReleaseAttempt.java
@@ -1,2 +1,18 @@
-public class IllegalReleaseAttempt {
+public class IllegalReleaseAttempt extends IllegalMonitorStateException{
+    /**
+     * Constructs an <code>IllegalMonitorStateException</code> with no
+     * detail message.
+     */
+    public IllegalReleaseAttempt() {
+    }
+
+    /**
+     * Constructs an <code>IllegalMonitorStateException</code> with the
+     * specified detail message.
+     *
+     * @param s the detail message.
+     */
+    public IllegalReleaseAttempt(String s) {
+        super(s);
+    }
 }
--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -1,2 +1,98 @@
-public class MyReentrantLock {
+import java.io.Closeable;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MyReentrantLock implements Lock{
+
+    public AtomicBoolean lock;
+    public Thread owner;
+    int ownerNumOfLocks;
+
+
+    public MyReentrantLock()
+    {
+        lock = new AtomicBoolean(false);
+    }
+
+
+    /**
+     *
+     */
+    @Override
+    public void acquire() {
+
+        while(true)
+        {
+                if(Thread.currentThread() == owner || lock.compareAndSet(false,true))
+                {
+                    try
+                    {
+                    owner = Thread.currentThread();
+                    ownerNumOfLocks++;
+                    return;
+                    }
+                    catch (Exception e)
+                    {
+                        owner=null;
+                        lock.set(false);
+                        return;
+                    }
+                }
+                }
+
+        }
+
+
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean tryAcquire() {
+        if(Thread.currentThread() == owner || lock.compareAndSet(false,true))
+        {
+            try
+            {
+                owner = Thread.currentThread();
+                ownerNumOfLocks++;
+                return true;
+            }
+            catch (Exception e)
+            {
+                owner=null;
+                lock.set(false);
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void release() {
+        Thread current = Thread.currentThread();
+        if(lock.get()==false || current != owner)
+        {
+//            throw new IllegalReleaseAttempt();
+            System.out.println("exception");
+        }
+        else{
+            ownerNumOfLocks--;
+            if(ownerNumOfLocks == 0)
+            {
+                lock.set(false);
+                owner=null;
+            }
+        }
+
+    }
+
+    /**
+
+     */
+    @Override
+    public void close()  {
+        this.release();
+    }
 }
